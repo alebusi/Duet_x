bb903d060b1bf8572d3ba24935443da4cba02953
alebusi <allebusi@gmail.com>
2020-11-15 17:19:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function posizionaMattone() {
   }
   xpos=randomIntFromInterval(0,2);
   document.getElementById(numcar[nx]).className="mattone";
-  document.getElementById(numcar[nx]).style.transform = "translate(-50%, -175%)";
+  document.getElementById(numcar[nx]).style.transform = "translate(-50%, -175px)";
   /*
   switch(nx) {
     case 1:
